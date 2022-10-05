bdc6e1f3290cef91060f1f63bdb624a602dc71a3
701tkdgns <701tkdgns@inu.ac.kr>
2022-10-06 01:34:00 +0900

    v0 code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import folder from './configuration/Folder.js'
+import Root from "./component/Root.js";
 
-
-const App = () =>{
-    return(
+const App = () => {
+    return (
         <>
-
+            <Root trees={folder} />
         </>
     )
 }
